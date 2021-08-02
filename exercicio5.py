3f26c406dcaad2d06d1d32eb878744096c4258b0
macielterra <macielterra@gmail.com>
2021-08-02 14:45:12 -0300

    Delete exercicio5.py

--- exercicio5.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('arquivos/numeros.csv','r', encoding='utf8', newline = '\r\n') as arquivo:
-    leitor = csv.reader(arquivo)
-    for x in leitor:
-        print ('Número de colunas:', len(x))
-        print(x)
